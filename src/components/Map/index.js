547b67d55a5925e795f81032555099b1418e1e8e
Metabaron <metabaron@Metabarons-MacBook-Pro.local>
2019-06-25 22:10:21 -0500

    Add basic transition to mapbox

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,31 +1,45 @@
 import React from 'react'
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, { FlyToInterpolator } from 'react-map-gl'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 export default class Map extends React.PureComponent {
   state = {
-    viewState: {
+    viewport: {
+      width: '100%',
+      height: '100vh',
       latitude: 19.3690477,
       longitude: -99.1792873,
-      zoom: 11
+      zoom: 5
     }
   }
 
-  onViewStateChange = ({ viewState }) => {
-    this.setState({ viewState })
+  onViewportChange = (viewport) => {
+    this.setState({ viewport })
+  }
+
+  mapLoaded = () => {
+    const { viewport } = this.state
+
+    this.setState({
+      viewport: {
+        ...viewport,
+        zoom: 10,
+        transitionDuration: 1000,
+        transitionInterpolator: new FlyToInterpolator(),
+      }
+    })
   }
 
   render() {
-    const { viewState } = this.state
+    const { viewport } = this.state
 
     return (
       <div className="map-container">
         <ReactMapGL
-          width="100%"
-          height="100vh"
-          viewState={viewState}
-          onViewStateChange={this.onViewStateChange}
+          {...viewport}
+          onLoad={this.mapLoaded}
+          onViewportChange={this.onViewportChange}
           mapboxApiAccessToken="pk.eyJ1IjoibWV0YWJhcm9uIiwiYSI6ImNqdnY2MjRmaDA4anA0OHM1NmRkY2Q5dXMifQ.LBG2c_Ip9INKczq1w1ivng"
           mapStyle="mapbox://styles/metabaron/cjxclp8ek0bpi1crpwlrsv0yn"
         />
